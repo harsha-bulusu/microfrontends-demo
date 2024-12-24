109aff821076e8a258a0e88bb49031d69642d891
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-12-24 20:40:31 +0530

    added prod config

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,20 @@
+const commonConfig = require('./webpack.common')
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJSON = require('../package.json')
+
+const prodConfig = {
+    mode: 'production',
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJSON.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
