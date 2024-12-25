7fb339bc9c882ca092c9d2e852c895b6dfdec44b
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-12-25 13:31:59 +0530

    updated prod config for container

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,24 +1,28 @@
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require('./webpack.common')
-const packageJSON = require('../package.json')
-const { merge } = require('webpack-merge')
-const domain = process.env.DOMAIN_NAME
+const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
+const { merge } = require('webpack-merge');
+const domain = process.env.DOMAIN_NAME;
 
-console.log(domain)
+console.log(domain);
+
+// Ensure there are no trailing slashes in the domain variable
+const sanitizedDomain = domain.replace(/\/$/, ''); 
+
 const productionConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
         publicPath: '/container/latest/'
     },
-    plugins:[
+    plugins: [
         new ModuleFederationPlugin({
             remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
+                marketing: `marketing@${sanitizedDomain}/marketing/latest/remoteEntry.js`
             },
             shared: packageJSON.dependencies
         })
     ]
-}
+};
 
-module.exports = merge(commonConfig, productionConfig)
+module.exports = merge(commonConfig, productionConfig);
