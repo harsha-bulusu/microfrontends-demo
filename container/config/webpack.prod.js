f3aac66dc3cc92d9f71f56e1dfcebe858fbaf4b5
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-12-25 14:10:05 +0530

    updated container workflow configs

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -2,8 +2,8 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common')
 const packageJSON = require('../package.json')
 const { merge } = require('webpack-merge')
-
 const domain = process.env.DOMAIN_NAME
+
 
 const productionConfig = {
     mode: 'production',
