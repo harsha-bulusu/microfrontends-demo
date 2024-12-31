dd66204cd46a4cd8d3a3953f3f91320b5777202f
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-12-31 14:13:32 +0530

    added app header

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 ReactDOM.render(
-    <div>
-        <div>Hi, There!</div>
-        <hr />
-        <MarketingApp />
-    </div>
+    <BrowserRouter>
+        <div>
+            <Header />
+            <MarketingApp />
+        </div>
+    </BrowserRouter>
     , document.getElementById("root"))
