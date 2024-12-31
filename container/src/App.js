919121778bfc7706758c261d77a95135461778c1
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-12-31 14:48:31 +0530

    refactored code: added routing to container

---
+++ container/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
+import Header from './components/Header';
+import MarketingApp from './components/MarketingApp';
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    )
+}
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MarketingApp from './components/MarketingApp'
-import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import App from './App';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            {/* <Header /> */}
-            <MarketingApp />
-        </div>
-    </BrowserRouter>
-    , document.getElementById("root"))
+ReactDOM.render(<App /> , document.getElementById("root"))
