3fcc00dcdd6c1b2aadbb72efbe05bf2b97f3b1b8
Sri Harsha <sriharsha@Sris-MacBook-Pro.local>
2024-12-31 14:52:56 +0530

    update import in container

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
